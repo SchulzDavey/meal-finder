7dacd1c2418219caa470d2ed4770fff8dabb81f2
Davey Schulz <davey.schulz@hotmail.com>
2023-11-19 12:21:24 +0100

    Added single category screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meal_finder/data/dummy_data.dart';
 import 'package:meal_finder/screens/categories.dart';
-import 'package:meal_finder/screens/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -24,10 +22,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const MealsScreen(
-        title: "Category",
-        meals: dummyMeals,
-      ),
+      home: const CategoriesScreen(),
     );
   }
 }
