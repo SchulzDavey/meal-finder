0592dac4434b6991e3620eb722a3650891228a6e
Davey Schulz <davey.schulz@hotmail.com>
2023-11-19 12:08:10 +0100

    dummy data added per category

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:meal_finder/data/dummy_data.dart';
 import 'package:meal_finder/screens/categories.dart';
+import 'package:meal_finder/screens/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -22,7 +24,10 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const CategoriesScreen(),
+      home: const MealsScreen(
+        title: "Category",
+        meals: dummyMeals,
+      ),
     );
   }
 }
